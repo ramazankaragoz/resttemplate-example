f515cab3d8cc3f624c7f0f3d1b2d87c26c49dd64
rkaragoz <ramazan.karagoz1@assistt.com>
2019-09-20 15:16:33 +0300

    PreAuthorizeValueProvider

--- src/main/java/com/example/resttemplate/resttemplateexample/config/PreAuthorizeValueProvider.java
+++ src/main/java/com/example/resttemplate/resttemplateexample/config/PreAuthorizeValueProvider.java
@@ -15,40 +15,58 @@ import java.util.stream.Collectors;
  * @author Ramazan Karagöz
  * @date 9/19/2019
  */
-
 public class PreAuthorizeValueProvider {
 
     private static final String PACKAGE_NAME = "com.example.resttemplate.resttemplateexample.controller";
-    private static final String VALUE_INDEX_PATTERN = "'";
+    private static final String HAS_AUTHORITY_VALUE_INDEX_PATTERN = "'";
+    private static final String HAS_ANY_AUTHORITY_VALUE_PATTERN = "hasAnyAuthority|\\W";
+    private static final String HAS_ANY_AUTHORITY_CONTAINS_VALUE="hasAnyAuthority";
     private static Set<String> values;
 
+    private PreAuthorizeValueProvider() {
+    }
 
     public static Set<String> getPreAuthorizeAnnotatedValues() {
 
-        String value;
         values = new HashSet<>();
         Reflections reflections = new Reflections(PACKAGE_NAME);
         Set<Class<?>> preAuthorizeClasses = reflections.getTypesAnnotatedWith(RestController.class);
 
-        if (preAuthorizeClasses != null && preAuthorizeClasses.size() > 0) {
+        if (preAuthorizeClasses != null && !preAuthorizeClasses.isEmpty()) {
             for (Class aClass : preAuthorizeClasses) {
-                for (Method method : aClass.getMethods()) {
-                    if (method.getAnnotation(PreAuthorize.class) != null && method.getAnnotation(PreAuthorize.class).value() != null) {
-                        value = method.getAnnotation(PreAuthorize.class).value();
-                        if (value.contains("hasAnyAuthority")) {
-                            List<String> hasAnyAuthorityValues = Arrays.asList(value.trim().split("hasAnyAuthority|\\W"));
-                            hasAnyAuthorityValues = hasAnyAuthorityValues.stream().filter(s -> s.length() > 0).collect(Collectors.toList());
-                            for (String v : hasAnyAuthorityValues) {
-                                values.add(v);
-                            }
-                        } else {
-                            value = value.substring(value.indexOf(VALUE_INDEX_PATTERN) + 1, value.lastIndexOf(VALUE_INDEX_PATTERN));
-                            if (value.length() > 0)
-                                values.add(value);
-                        }
-                    }
+                List<Method> methods=Arrays.asList(aClass.getDeclaredMethods());
+                values = getPreAuthorizeAnnotatedMethodValues(methods);
+            }
+        }
+
+        return values;
+    }
+
+    private static Set<String> getPreAuthorizeAnnotatedMethodValues(List<Method> methods) {
+
+        if (methods != null && !methods.isEmpty()) {
+            for (Method method : methods) {
+                if (method.getAnnotation(PreAuthorize.class) != null && method.getAnnotation(PreAuthorize.class).value() != null) {
+                    values=findFilteredAllValue(method.getAnnotation(PreAuthorize.class).value());
                 }
             }
+        }
+
+        return values;
+    }
+
+    private static Set<String> findFilteredAllValue(String value) {
+
+        if (value.contains(HAS_ANY_AUTHORITY_CONTAINS_VALUE)) {
+            List<String> hasAnyAuthorityValues = Arrays.asList(value.trim().split(HAS_ANY_AUTHORITY_VALUE_PATTERN));
+            hasAnyAuthorityValues = hasAnyAuthorityValues.stream().filter(s -> s.length() > 0).collect(Collectors.toList());
+            for (String v : hasAnyAuthorityValues) {
+                values.add(v);
+            }
+        } else {
+            value = value.substring(value.indexOf(HAS_AUTHORITY_VALUE_INDEX_PATTERN) + 1, value.lastIndexOf(HAS_AUTHORITY_VALUE_INDEX_PATTERN));
+            if (value.length() > 0)
+                values.add(value);
         }
 
         return values;
